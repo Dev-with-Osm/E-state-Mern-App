773d7edcc24f0578921a316ff76510f8c267860c
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-01-17 14:26:42 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
+const mongoUrl = process.env.MONGO_URL;
 
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
