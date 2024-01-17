60c50c908a5aacc1731dd879466797e4c9e21494
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-01-17 22:28:08 +0100

    ceate sign up api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/user.route.js");
+const authRouter = require("./routes/auth.route.js");
 dotenv.config();
 const app = express();
+app.use(express.json());
 const mongoUrl = process.env.MONGO_URL;
 
 mongoose
@@ -16,6 +18,7 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
