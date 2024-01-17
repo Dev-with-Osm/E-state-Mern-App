8b7c7b9bd32e46cc5ef47f049ca7011cb138503d
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-01-17 16:00:09 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRouter = require("./routes/user.route.js");
 dotenv.config();
 const app = express();
 const mongoUrl = process.env.MONGO_URL;
@@ -13,6 +14,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use("/api/user", userRouter);
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
