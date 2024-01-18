a9d1259a145f9e0b4f0dd5117cd41628323dbcc5
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-01-18 13:56:31 +0100

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
+const { response } = require("express");
 const User = require("../models/user.model.js");
+const { errorHandler } = require("../utils/error.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports.signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +13,24 @@ module.exports.signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("user created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong cedentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
