5e22d8ea1f0d12ceedd7867456dd4d6d951f9e59
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-01-23 11:29:49 +0100

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,35 @@
+const User = require("../models/user.model");
+const { errorHandler } = require("../utils/error");
+const bcrypt = require("bcryptjs");
+
 module.exports.test = (req, res) => {
   res.json({
     message: "hello api!",
   });
+};
+module.exports.updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/user.route.js");
 const authRouter = require("./routes/auth.route.js");
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 const mongoUrl = process.env.MONGO_URL;
 
 mongoose
