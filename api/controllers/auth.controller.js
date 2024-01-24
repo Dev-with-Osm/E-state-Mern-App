11dfd91d1be756e7d74599e491d059cef6a66868
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-01-24 11:22:51 +0100

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,3 +70,12 @@ module.exports.google = async (req, res) => {
     next(error);
   }
 };
+
+module.exports.signOut = (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been signed out");
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -100,6 +103,22 @@ export default function Profile() {
     }
   };
 
+  const handleSignOutUser = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-center text-3xl font-semibold my-7">Profile</h1>
@@ -168,7 +187,12 @@ export default function Profile() {
         >
           Delete account
         </span>
-        <span className="text-red-800 cursor-pointer">Sign Out</span>
+        <span
+          onClick={handleSignOutUser}
+          className="text-red-800 cursor-pointer"
+        >
+          Sign Out
+        </span>
       </div>
 
       <p className="text-red-800 font-semibold mt-5">{error ? error : ""}</p>
