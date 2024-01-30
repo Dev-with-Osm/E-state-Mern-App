1b86a0de9c02ec19660c6c3f57dc01e72cb98137
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-01-30 09:48:46 +0100

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const Listing = require("../models/listing.model");
 const User = require("../models/user.model");
 const { errorHandler } = require("../utils/error");
 const bcrypt = require("bcryptjs");
@@ -42,4 +43,17 @@ module.exports.deleteUser = async (req, res, next) => {
     res.clearCookie("access_token");
     res.status(200).json("User have been deleted!");
   } catch (error) {}
+};
+
+module.exports.getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings"));
+  }
 };
