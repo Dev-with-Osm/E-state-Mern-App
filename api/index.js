f2ec5ad1e906153f6d1706a3ccdd14b7577788dd
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-02-02 00:56:20 +0100

    change the dirname

--- api/index.js
+++ api/index.js
@@ -22,16 +22,16 @@ mongoose
     console.log(err);
   });
 
-// const __dirname = path.resolve();
+const dirname = path.resolve();
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(dirname, "client", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
